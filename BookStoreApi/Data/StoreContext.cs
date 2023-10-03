c3e5c3619856ea66e8afc0b851b806d0b40b66f2
gintex232 <amahuo.godwin232@gmail.com>
2023-10-04 00:04:30 +0100

    database created

--- BookStoreApi/Data/StoreContext.cs
+++ BookStoreApi/Data/StoreContext.cs
@@ -1,6 +1,14 @@
-﻿namespace BookStoreApi.Data
+﻿using BookStoreApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Data
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
